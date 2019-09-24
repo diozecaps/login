781967cbcb6dd4065339136c0c85723125737c6b
diozecaps <jose_v_dias@estudante.sc.senai.br>
2019-09-24 20:57:41 -0300

    formulário de Login
    close #1

--- index.php
+++ index.php
@@ -34,8 +34,39 @@
                     Entrar no Sistema
                 </h2>
                     <form action="#" method="post" class="p-2" id="formLogin">
+
                         <div class="form-group">
-                            <input type="text" name="" id="">
+                            <input type="text" name="nomeUsuario" id="nomeUsuario" placeholder="Nome de Usuário" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <input type="password" name="senhaUsuario" id="senhaUsuario" placeholder="Senha" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" name="lembrar" id="lembrar" class="custom-control-input">
+                                <label for="lembrar" class="custom-control-label">
+                                    Lembrar de mim.
+                                </label>
+
+                                <a href="#" class="float-right" id="btnEsqueci">
+                                    Esqueci a senha!
+                                </a>
+
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="submit" value="::Entrar::" name="btnEntrar" id="btnEntrar" class="btn btn-primary btn-block">
+                        </div>
+
+                        <div class="form-group">
+                            <p text-center>
+                                Novo Usuário? <a href="#">
+                                    Registre-se aqui!
+                                </a>
+                            </p>
                         </div>
                     </form>
             </div>
