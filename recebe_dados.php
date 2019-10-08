750e34149bd27f7442a4a324c90f2d6c0d4e1dc4
diozecaps <jose_v_dias@estudante.sc.senai.br>
2019-10-07 21:04:45 -0300

    recebe dados
    att 3.0

--- recebe_dados.php
+++ recebe_dados.php
@@ -1,29 +1,23 @@
 <?php
-
-//Teste se existe ação 
+//Teste se existe a ação
 if (isset($_POST['action'])) {
-
-    if ($_POST['action'] == 'cadastro') {
+    if ($_POST['action'] = 'cadastro') {
         //Teste se ação é igual a cadastro
         echo "\n<p>cadastro</p>";
-        echo "\n <pre>";
+        echo "\n<pre>"; //Pre-Formatar
         print_r($_POST);
         echo "\n</pre>";
-    } else if ($_POST['action'] == 'login') {
+    } else if ($_POST['action'] = 'login') {
         //Senão, teste se ação é login
-        echo "\n<p>login</p>";
-        echo "\n <pre>";
-        print_r($_POST);
-        echo "\n</pre>";
-    } else if ($_POST['action'] == 'senha') {
+
+    } else if ($_POST['action'] = 'senha') {
         //Senão, teste se ação é recuperar senha
-        echo "\n<p>senha</p>";
-        echo "\n <pre>";
-        print_r($_POST);
-        echo "\n</pre>";
+
     } else {
         header("location:index.php");
     }
 } else {
+    //Redirecionando para index.php, negando o acesso
+    //a esse arquivo diretamente.
     header("location:index.php");
 }
