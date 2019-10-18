747ca81e9abfbdeb8bfe934a7db252fcf4a43aa7
diozecaps <jose_v_dias@estudante.sc.senai.br>
2019-10-18 19:07:54 -0300

    att

--- configBD.php
+++ configBD.php
@@ -1,15 +1,12 @@
 <?php
-
-//ConfigBD.php
+//configBD.php
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
 $dbname = "senai";
-
 $connect = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-
-if($connect ->connect_error){
-    die("Não foi possivel conectar ao banco de dados:" . $connect ->connect_error);
+if($connect->connect_error){
+    die("Não foi possível conectar ao banco de dados: " . 
+    $connect->connect_error);
 }
-
-echo"<h1>Sucesso, banco de dados conectado</h1>";
+#echo "<h1>Sucesso, banco de dados conectado</h1>";
